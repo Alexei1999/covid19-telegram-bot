52670c86ec3dc2beb827d7e21cee9bb74a359aad
Alexei1999 <Shulzhickij.ao.17@gmail.com>
2020-04-18 04:14:09 +0300

    minor changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ class Monitor {
     constructor(ctx, country = 'Belarus') {
         this.target = country
         this.chatId = ctx.message.chat.id
-        console.log(`Target country - ${this.target}`)
+        console.log(`target country - ${this.target}`)
         this.wait()
     }
     wait() {
@@ -21,12 +21,13 @@ class Monitor {
         let month = target.getMonth()
         let day = target.getDate()
         target = new Date(year, month, day).setHours(target.getHours() + 1) - Date.now()
+
         console.log(`next step in ${target}ms`)
         this.timer = setTimeout(() => { this.check() }, target)
     }
     async check() {
         let data = await this.fetchData()
-        data == this.last || this.update(data)
+            (!this.last || data[0] != this.last[0]) && this.update(data)
 
         this.wait()
     }
@@ -34,20 +35,28 @@ class Monitor {
         console.log('fetching...')
         try {
             const $ = await got(url).then(res => cheerio.load(res.body))
-            return $(`tr:contains("${target}")`).first().text().split('\n').filter(s => s).concat(Date.now())
+            return $(`tr:contains("${this.target}")`).first().text().split('\n').filter(s => s).concat(Date.now())
         }
         catch (e) {
             console.log(e.message)
         }
     }
+
     async update(data) {
         console.log('sending message...')
-        data.push(new Date(data.pop()))
-        let message = `${flag(data[0])} 😷${data[1]}${this.last ? '(+' + (data[1] - this.last[1]) + ')' : ''} 💀${data[2]} ❤️${data[3]} | updated: ${data[5]}`
 
-        fs.appendFileSync('./log.txt', dara.join('|') + '\n')
+        fs.appendFileSync('./log.txt', data.join('|') + '\n')
+        let date = (new Date(data.pop()).toString().replace(/GMT.\d\d\d\d./, ''))
+        let message = `${flag(data[0])}   😷${data[1]}${insertDiff(data, this.last, 1)}  💀${data[2]}${insertDiff(data, this.last, 2)}  ❤️${data[3]}${insertDiff(data, this.last, 3)}\n${date}`
+
         bot.telegram.sendMessage(this.chatId, message, [{ disable_notification: true }])
         this.last = data
+
+        function insertDiff(a, b, n) {
+            if (!a || !b || a[n] == b[n]) return ''
+            let c = a[n] > b[n] ? '+' : ''
+            return `(${c}${a[n].replace(',', '') - b[n].replace(',', '')})`
+        }
     }
     stop() {
         clearTimeout(this.timer)
@@ -55,7 +64,9 @@ class Monitor {
 }
 
 bot.start((ctx) => {
-    console.log('Started monitoring...')
+    console.log('started monitoring...')
     new Monitor(ctx)
 })
-bot.launch()
+bot.launch().then(() => {
+    console.log('Bot started listening...')
+})
